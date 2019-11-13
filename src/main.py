0d45bcd2353e90a84e7f7fcc3e9eb9dd3721d856
hrubanj <jiri.hruban@email.cz>
2019-11-13 16:43:38 +0100

    Replaced lists with generators

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from keboola import docker
 import xmltodict
-from itertools import chain
+from itertools import chain, tee
 import logging
 import datetime
 import csv
@@ -86,20 +86,21 @@ if __name__ == '__main__':
 
     # download xml files that are xml, were not present in the last download, have allowed pattern
     # or specifically enumerated files
-    files_to_download_1 = [file for file in esol_bucket.objects.all()
-                           if file.key.endswith(".xml")
+    files_to_download = (file for file in esol_bucket.objects.all()
+                           if (file.key.endswith(".xml")
                            and any(name_pattern in file.key for name_pattern in allowed_file_patterns)
                            and all(name_pattern not in file.key for name_pattern in forbidden_file_patterns)
                            and file.last_modified.replace(tzinfo=None) >
-                           datetime.datetime.strptime(last_processed_timestamp, "%Y-%m-%d %H:%M:%S")
-                           ]
+                           datetime.datetime.strptime(last_processed_timestamp, "%Y-%m-%d %H:%M:%S"))
+                           or (file.key in input_fileset)
+                         )
+    # we need to reuse the generator
+    # copy it to save memory
+    files_to_download, files_to_download_backup = tee(files_to_download)
 
-    files_to_download_2 = [file for file in esol_bucket.objects.all() if file.key in input_fileset]
-
-    files_to_download = files_to_download_1 + files_to_download_2
 
     max_timestamp_this_run = [{"max_timestamp_this_run": max(file.last_modified
-                                                             for file in files_to_download).replace(
+                                                             for file in files_to_download_backup).replace(
         tzinfo=None).strftime("%Y-%m-%d %H:%M:%S")}]
 
     logging.info("Collected files to download.")
@@ -118,6 +119,8 @@ if __name__ == '__main__':
             esol_bucket.download_fileobj(file.key, f)
 
         logging.info(f"File {filepath.split('/')[-1]} downloaded.")
+
+    del files_to_download, session, s3, esol_bucket
 
     with open(f"{kbc_datadir}out/tables/esolutions_last_timestamp.csv", 'w+', encoding="utf-8") as f:
         dict_writer = csv.DictWriter(f, ["max_timestamp_this_run"])
